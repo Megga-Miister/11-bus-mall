558b2d2dc6a05a072f110b7cb383de2a4b3ab4a0
Megga-Miister <mtriplett@ISOLALT-64.localdomain>
2018-11-07 13:29:37 -0800

    formatted images to a standard square shape. got images to randomly display

--- app.js
+++ app.js
@@ -17,6 +17,27 @@ function Product(name, src) {
  products.push(this);
 }
 
+var bag = new Product ('Bag', './assets/bag.jpg'); 
+var banana = new Product ('Banana', './assets/banana.jpg');
+var bathroom = new Product ('Bathroom', './assets/bathroom.jpg');
+var boots = new Product ('Boots', './assets/boots.jpg');
+var breakfast = new Product ('Breakfast', './assets/breakfast.jpg');
+var bubblegum = new Product ('Bubblegum', './assets/bubblegum.jpg');
+var chair = new Product ('Chair', './assets/chair.jpg');
+var cthulhu = new Product ('Cthulhu', './assets/cthulhu.jpg');
+var dogduck = new Product ('Dog-Duck', './assets/dog-duck.jpg');
+var dragon = new Product ('Dragon', './assets/dragon.jpg');
+var pen = new Product ('Pen', './assets/pen.jpg');
+var petsweep = new Product ('Pet-Sweep', './assets/pet-sweep.jpg');
+var scissors = new Product ('Scissors', './assets/scissors.jpg');
+var shark = new Product ('Shark', './assets/shark.jpg');
+var sweep = new Product ('Sweep', '/assets/sweep.png');
+var tauntaun = new Product ('Tauntaun', './assets/tauntaun.jpg');
+var unicorn = new Product ('Unicorn', './assets/unicorn.jpg');
+var usb = new Product ('USB', './assets/usb.gif');
+var watercan = new Product ('Water-Can', './assets/water-can.jpg');
+var wineglass = new Product ('Wine-Glass', './assets/wine-glass.jpg');
+
 function getRandomIndex() {
     var randomIndex = Math.floor(Math.random() * products.length);
     return randomIndex;
@@ -26,8 +47,10 @@ function getUniqueImages() {
   displayProduct1 = products[getRandomIndex()].src;
   displayProduct2 = products[getRandomIndex()].src;
   displayProduct3 = products[getRandomIndex()].src;
-  threeProductDisplay = [displayProduct1, displayProduct2, displayProduct3];
-  return threeProductDisplay;
+
+  threeProductDisplay.push(displayProduct1);
+  threeProductDisplay.push(displayProduct2);
+  threeProductDisplay.push(displayProduct3);
 
   // if (displayProduct1 != displayProduct2 && displayProduct2 != displayProduct3){
   //   threeProductDisplay.push(displayProduct1, displayProduct2, displayProduct3);
@@ -37,31 +60,35 @@ function getUniqueImages() {
 
   //     } while (displayProduct1 === displayProduct2);
   // }
-}
 
+  return threeProductDisplay;
+}
+console.log(threeProductDisplay);
 
 function renderImages() {
-  mainEl = document.getElementById('main-content');
-  divEl = document.createElement('div'); //create element for div
-  leftImgEl = document.createElement('img'); //create elements for three imagies
+  getUniqueImages();
+
+  var mainEl = document.getElementById('main-content');
+  var divEl = document.createElement('div'); //create element for div
+  var leftImgEl = document.createElement('img'); //create elements for three imagies
   leftImgEl.id = 'leftImg';
-  centerImgEl = document.createElement('img');
+  var centerImgEl = document.createElement('img');
   centerImgEl.id = 'centerImg';
-  rightImgEl = document.createElement('img');
+  var rightImgEl = document.createElement('img');
   rightImgEl.id = 'rightImg';
 
-  leftImgEl.textContent = 
-
-  leftImgEl.src = leftImgEl.src;
-  centerImgEl.src = centerImgEl.src;
-  rightImgEl.src = rightImgEl.src;
+  leftImgEl.src = threeProductDisplay[0];
+  centerImgEl.src = threeProductDisplay[1];
+  rightImgEl.src = threeProductDisplay[2];
 
   mainEl.appendChild(divEl);  //append div to mainEl
   divEl.appendChild(leftImgEl);  //append images to div
   divEl.appendChild(centerImgEl);
   divEl.appendChild(rightImgEl);
+
 }
 
+renderImages();
 
 //   addClickTracker: function() {
 
@@ -77,26 +104,4 @@ function renderImages() {
 
 // })()
 
-var bag = new Product ('Bag', '/assets/bag.jpg'); 
-var banana = new Product ('Banana', '/assets/banana.jpg');
-var bathroom = new Product ('Bathroom', '/assets/bathroom.jpg');
-var boots = new Product ('Boots', '/assets/boots.jpg');
-var breakfast = new Product ('Breakfast', '/assets/breakfast.jpg');
-var bubblegum = new Product ('Bubblegum', '/assets/bubblegum.jpg');
-var chair = new Product ('Chair', '/assets/chair.jpg');
-var cthulhu = new Product ('Cthulhu', '/assets/cthulhu.jpg');
-var dogduck = new Product ('Dog-Duck', '/assets/dog-duck.jpg');
-var dragon = new Product ('Dragon', '/assets/dragon.jpg');
-var pen = new Product ('Pen', '/assets/pen.jpg');
-var petsweep = new Product ('Pet-Sweep', '/assets/pet-sweep.jpg');
-var scissors = new Product ('Scissors', '/assets/scissors.jpg');
-var shark = new Product ('Shark', '/assets/shark.jpg');
-var sweep = new Product ('Sweep', '/assets/sweep.png');
-var tauntaun = new Product ('Tauntaun', '/assets/tauntaun.jpg');
-var unicorn = new Product ('Unicorn', '/assets/unicorn.jpg');
-var usb = new Product ('USB', '/assets/usb.gif');
-var watercan = new Product ('Water-Can', '/assets/water-can.jpg');
-var wineglass = new Product ('Wine-Glass', '/assets/wine-glass.jpg');
-
 console.log(products);
-console.log(getUniqueImages());
